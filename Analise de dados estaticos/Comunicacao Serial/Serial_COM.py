f3c00b882f698adc7ec8c13afec557889a68722e
ViniciusCMB <viniciusmonnerat958.com>
2023-07-21 11:01:51 -0300

    rd

--- Analise de dados estaticos/Comunicacao Serial/Serial_COM.py
+++ Analise de dados estaticos/Comunicacao Serial/Serial_COM.py
@@ -1,6 +1,7 @@
 import serial
 from datetime import datetime
 
+# def get_data(amonunt):
 while True: #Loop para a conexão com o Arduino
     try:  #Tenta se conectar, se conseguir, o loop se encerra
         arduino = serial.Serial('/dev/ttyUSB0', 9600)
@@ -12,11 +13,15 @@ while True: #Loop para a conexão com o Arduino
 while True: #Loop principal
     msg = str(arduino.readline()) #Lê os dados em formato de string
     now = datetime.now()
+    timestamp = now.timestamp()
     current_time = now.strftime("%H:%M:%S")
     msg = msg[2:-5] #Fatia a string
     print(msg) #Imprime a mensagem
     arq = open("dados.txt",'a') 
+    arq.write(str(timestamp)+';')
     arq.write(current_time+';')
     arq.write(msg+'\n')
     arq.close()
     arduino.flush() #Limpa a comunicação
+
+    # return None
