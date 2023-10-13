1a9cb6ad672d6dd7079d5827def7dc130fe34273
ViniciusCMB <viniciusmonnerat958.com>
2023-10-12 22:53:02 -0300

    Modo PDF

--- Analise de dados estaticos/Integral_Grafico.py
+++ Analise de dados estaticos/Integral_Grafico.py
@@ -4,6 +4,7 @@ from matplotlib.ticker import (AutoMinorLocator)
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import customtkinter as ctk
 import easygui
+from fpdf import FPDF
 from PIL import Image
 
 
@@ -69,31 +70,73 @@ def calc_integral():
     list_result['integral'] = nd_simp(
         yi, calc_h(xi[len(xi)-1], xi[0], len(xi)-1))
 
-    # print('Integral método 2ª Regra de Simpson composta = {:.4f}'.format(list_result['integral'])+'N*s')
-
     list_result['medioE'] = (1/(xi[len(xi)-1]-xi[0]))*list_result['integral']
-    # print('\nEmpuxo médio: {:.4f}'.format(list_result['medioE'])+'N')
+
     return None
 
 
 def graph():
 
     def save(file):
-        f = open(file+'_'+list_result['inicio'], "a")
-        f.write('Impulso Total (N*s) ≈ {:.4f}'.format(list_result['integral']) +
-                '\nEmpuxo médio (N) ≈ {:.4f}'.format(list_result['medioE']) +
-                '\nEmpuxo máximo (N) ≈ {:.4f}'.format(list_result['maxyE'])+', em t={:.4f}'.format(list_result['maxxE']) +
-                '\nQuantidade de pontos: '+list_result['pontos'] +
-                '\nTempo de queima (s) ≈ {:.3f}'.format(list_result['duracao']) +
-                '\nHorário do teste: '+list_result['inicio'])
-        f.close()
-        plt.savefig(file+'_'+list_result['inicio']+'.png')
+        plt.savefig(file+'.png')
+
+        class PDF(FPDF):
+            def header(self):
+                # Logos
+                self.image(
+                    '/home/vinicius/Área de Trabalho/Serra_Rocketry/Analise de dados estaticos/LOGO - ALTERNATIVA.png', 210-47, -5, 50)
+                self.image(
+                    '/home/vinicius/Área de Trabalho/Serra_Rocketry/Analise de dados estaticos/logomarca-uerj-300x300.png', 2, 2, 35)
+
+                self.set_font('Arial', 'B', 25)
+                # Move to the right
+                self.cell(80)
+                # Title
+                self.cell(30, 25, 'Relatório de Teste Estático', 0, 0, 'C')
+                # Line break
+                self.ln(20)
+                self.set_fill_color(r=43, g=18, b=76)
+                self.set_y(45)
+                self.cell(0, 1, ' ', 0, 1, 'C', 1)
+
+            # Page footer
+            def footer(self):
+                # Position at 1.5 cm from bottom
+                self.set_y(-15)
+                # Arial italic 8
+                self.set_font('Arial', 'I', 8)
+                # Page number
+                self.cell(0, 10, 'Página ' + str(self.page_no()) +
+                          '/{nb}' + ' - Equipe Serra Rocketry', 0, 0, 'C')
+
+        # Instantiation of inherited class
+        pdf = PDF()
+        pdf.alias_nb_pages()
+        pdf.add_page()
+        pdf.set_xy(15, 50)
+        pdf.set_font('Times', '', 18)
+        pdf.multi_cell(0, 10, 'Data do teste: ' +
+                       str(list_result['data'])+'\nHora do teste: '+str(list_result['inicio']))
+
+        pdf.set_xy(15, 65)
+        pdf.set_font('Times', '', 14)
+        pdf.multi_cell(0, 8, '\nImpulso Total (N*s) = {:.3f}'.format(list_result['integral']) +
+                       '\nEmpuxo Médio (N) = {:.3f}'.format(list_result['medioE']) +
+                       '\nEmpuxo Máximo (N) = {:.3f}'.format(list_result['maxyE']) +
+                       '\nTempo aproximado de queima (s)= {:.1f}'.format(list_result['duracao']), 0, 1)
+
+        pdf.set_fill_color(r=43, g=18, b=76)
+        pdf.set_y(110)
+        pdf.cell(0, 1, ' ', 0, 1, 'C', 1)
+        pdf.image(file+'.png', (210/2)-90, 120, 180, 140)
+        pdf.output(file+'.pdf', 'F')
+
         return None
-    
+
     def open_matplot():
 
         return plt.show()
-    
+
     def save_path():
 
         return easygui.filesavebox()
@@ -103,8 +146,8 @@ def graph():
     plt.rcParams["figure.figsize"] = (8, 6)
     fig, ax1 = plt.subplots()
 
-    ax1.set_title('Gráfico Empuxo vs Tempo ' +
-                  list_result['inicio'], fontsize=15)
+    ax1.set_title('Gráfico Empuxo vs Tempo -' +
+                  list_result['data'] + '-' + list_result['inicio'], fontsize=15)
     ax1.grid()
     max_x = 0.0
     max_y = 0.0
@@ -122,34 +165,14 @@ def graph():
         linha, = ax1.plot(t, p(t), c='red')
 
     linha.set_label('Interpolação')
-
-    # print('Empuxo máximo: {:.4f}'.format(list_result['maxyE'])+'N, em t={:.4f}'.format(list_result['maxxE']))
     ax1.scatter(max_x, max_y, c='purple', label='Máximo')
     ym = [(list_result['medioE'])]*(len(xi))
     ax1.plot(xi, ym, c='green', label='Empuxo médio')
-
-    # r = input('\nDeseja buscar um valor específico no tempo?\n"S" para sim e "N" para não\nResposta: ')
-    # if r == 'S' or r == 's':
-    #     a = float(input(f'Informe o instante desejado no intervalo [{min(xi)},{max(xi)}]: '))
-    #     for key, value in sn.items():
-    #         def p(x):
-    #             return eval(value['eq'])
-    #         t = npy.linspace(*value['dominio'],100)
-    #         for i in range(len(t)-1):
-    #             if a > t[i] and a < t[i+1]:
-    #                 tx = p(a)
-    #                 print(f'O Empuxo estimado é {tx:.4f}N no instante t={a}')
-    #                 plt.scatter(a, tx, c='blue', label='Interesse')
-    #                 break
-
-    # ax1.plot(xi, yi, c='blue', label=None)
     ax1.scatter(xi, yi, c='black', label='Pontos')
-
     ax1.set_ylabel('Empuxo (N)', fontsize=12)
     # ax1.set_xticks(xi, h, rotation=45)
     ax1.set_xlabel('Tempo (s)', fontsize=12)
-    # ax1.text(max(xi), max(yi), r'Impulso Total (N*s) ≈ {:.4f}'.format(list_result['integral'])+'\nEmpuxo médio (N) ≈ {:.4f}'.format(list_result['medioE'])+'\nEmpuxo máximo (N) ≈ {:.4f}'.format(list_result['maxyE']), horizontalalignment='right', verticalalignment='top', fontsize=10)
-    ax1.yaxis.set_minor_formatter('{x:.3f}')
+    ax1.yaxis.set_minor_formatter('{x:.2f}')
     ax1.yaxis.set_major_formatter('{x:.2f}')
     ax1.yaxis.set_minor_locator(AutoMinorLocator())
     ax1.tick_params(which='both', width=2)
@@ -162,43 +185,45 @@ def graph():
     root.title("Painel de Análise")
     root.maxsize(1400,  900)
 
-    Frame = ctk.CTkFrame(master=root,  width=1200,  height=800,
-                         fg_color='#8989b8', corner_radius=0)
+    Frame = ctk.CTkFrame(master=root,  width=1200,  height=900,
+                         fg_color='#473E66', corner_radius=0)
     Frame.pack(fill='both',  padx=5,  pady=5,  expand=True)
 
     plotframe = ctk.CTkFrame(master=Frame, width=800,
-                             height=600, fg_color='#656693', corner_radius=5)
+                             height=600, fg_color='#F5D7DB', corner_radius=5)
     plotframe.pack(padx=5, pady=15, side='left')
     canvas = FigureCanvasTkAgg(fig, master=plotframe)
     canvas.draw()
     canvas.get_tk_widget().place(relx=0, rely=0)
 
-    infos = ctk.CTkLabel(master=Frame, text='Impulso Total (N*s) ≈ {:.4f}'.format(list_result['integral']) +
-                         '\nEmpuxo médio (N) ≈ {:.4f}'.format(list_result['medioE']) +
-                         '\nEmpuxo máximo (N) ≈ {:.4f}'.format(list_result['maxyE']) +
+    infos = ctk.CTkLabel(master=Frame, text='Data do teste: '+list_result['data'] +
+                         '\nHorário do teste: '+list_result['inicio'] +
+                         '\nImpulso Total (N*s) ≈ {:.3f}'.format(list_result['integral']) +
+                         '\nEmpuxo médio (N) ≈ {:.3f}'.format(list_result['medioE']) +
+                         '\nEmpuxo máximo (N) ≈ {:.3f}'.format(list_result['maxyE']) +
                          '\nQuantidade de pontos: '+list_result['pontos'] +
-                         '\nTempo de queima (s) ≈ {:.3f}'.format(list_result['duracao']) +
-                         '\nHorário do teste: '+list_result['inicio'],
-                         fg_color='#656693',
+                         '\nTempo de queima (s) ≈ {:.1f}'.format(
+                             list_result['duracao']),
+                         fg_color='#522B5B',
                          text_color='white',
                          corner_radius=5,
                          height=200,
                          width=400,
-                         font=('Roboto', 20, 'bold'))
-    infos.pack(padx=5, pady=10, side='top')
+                         font=('Roboto', 18, 'bold'))
+    infos.pack(padx=5, pady=14, side='top')
 
-    save_btn = ctk.CTkButton(master=Frame, corner_radius=10, height=50, width=150, font=('Roboto', 15), fg_color='#7777a6',
-                             hover_color='#acabdd', border_color='black', border_width=2, text='Salvar análise', command=lambda: [save(save_path())])
+    save_btn = ctk.CTkButton(master=Frame, corner_radius=10, height=50, width=150, font=('Roboto', 15), fg_color='#522B5B',
+                             hover_color='#2B124C', border_color='black', border_width=2, text='Gerar Relatório', command=lambda: [save(save_path())])
     save_btn.pack(padx=5, pady=10, side='top')
 
-    matplot_btn = ctk.CTkButton(master=Frame, corner_radius=10, height=50, width=150, font=('Roboto', 15), fg_color='#7777a6',
-                             hover_color='#acabdd', border_color='black', border_width=2, text='Abrir Matplotlib', command=lambda: [open_matplot()])
+    matplot_btn = ctk.CTkButton(master=Frame, corner_radius=10, height=50, width=150, font=('Roboto', 15), fg_color='#522B5B',
+                                hover_color='#2B124C', border_color='black', border_width=2, text='Abrir o Gráfico', command=lambda: [open_matplot()])
     matplot_btn.pack(padx=5, pady=10, side='top')
 
-    your_image = ctk.CTkImage(light_image=Image.open('Analise de dados estaticos/Comunicacao Serial/LOGO - ALTERNATIVA.png'), size=(250, 250))
+    your_image = ctk.CTkImage(light_image=Image.open(
+        '/home/vinicius/Área de Trabalho/Serra_Rocketry/Analise de dados estaticos/LOGO - ALTERNATIVA.png'), size=(250, 250))
     label = ctk.CTkLabel(master=Frame, image=your_image, text='')
     label.pack(padx=5, pady=5, side='bottom')
-
 
     root.update()
 
@@ -209,14 +234,17 @@ def select():
 
     return easygui.fileopenbox()
 
+
 yi = []
 xii = []
 h = []
+data = []
 arq = open(select(), 'r').read()
 lines = arq.split('\n')
 for line in lines:
     if len(line) > 1:
-        z, x, y = line.split(';')
+        d, z, x, y = line.split(';')
+        data.append(d)
         h.append(z)
         yi.append(float(x)*9.81)
         xii.append(float(y)/1000)
@@ -224,7 +252,7 @@ for line in lines:
 xi = [xii[i]-xii[0] for i in range(len(xii))]
 
 list_result = {'integral': 0.0, 'maxxE': 0.0, 'maxyE': 0.0, 'medioE': 0.0,
-               'pontos': str(len(yi)), 'duracao': xi[len(xi)-1]-xi[0], 'inicio': h[0]}
+               'pontos': str(len(yi)), 'duracao': xi[len(xi)-1]-xi[0], 'inicio': h[0], 'data': data[0]}
 
 calc_integral()
 graph()
