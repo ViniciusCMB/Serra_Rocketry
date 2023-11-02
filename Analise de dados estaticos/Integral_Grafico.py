de1d300f4f2693803fdcd811dcb3861f8fa77ac1
Vinicius Carvalho Monnerat Bandeira <108745449+ViniciusCMB@users.noreply.github.com>
2023-11-02 11:14:56 -0300

    Update

--- Analise de dados estaticos/Integral_Grafico.py
+++ Analise de dados estaticos/Integral_Grafico.py
@@ -65,6 +65,22 @@ def spline(xi, yi):
     return s
 
 
+def mmq(xi, yi):
+    n = len(xi)
+    sum_x = sum(xi)
+    sum_x2 = sum([i**2 for i in xi])
+    sum_y = sum(yi)
+    sum_xy = sum([xi[i]*yi[i] for i in range(n)])
+    A = [[n, sum_x], [sum_x, sum_x2]]
+    B = [sum_y, sum_xy]
+
+    return npy.linalg.solve(A, B)
+
+
+def calc_mmq(x, c1, c0):
+    return (c1*x)+c0
+
+
 def calc_integral():
 
     list_result['integral'] = nd_simp(
@@ -72,82 +88,150 @@ def calc_integral():
 
     list_result['medioE'] = (1/(xi[len(xi)-1]-xi[0]))*list_result['integral']
 
+    list_result['classificacao'] = classe(
+        list_result['integral'], list_result['medioE'], list_result['duracao'])
+
     return None
+
+
+def classe(total, medio, tempo):
+    if total <= 0.0625:
+        designation = '1/4A'
+    elif total > 0.625 and total <= 1.25:
+        designation = '1/2A'
+    elif total > 1.25 and total <= 2.5:
+        designation = 'A'
+    elif total > 2.5 and total <= 5:
+        designation = 'B'
+    elif total > 5 and total <= 10:
+        designation = 'C'
+    elif total > 10 and total <= 20:
+        designation = 'D'
+    elif total > 20 and total <= 40:
+        designation = 'E'
+    elif total > 40 and total <= 80:
+        designation = 'F'
+    elif total > 80 and total <= 160:
+        designation = 'G'
+    elif total > 160 and total <= 320:
+        designation = 'H'
+    elif total > 320 and total <= 640:
+        designation = 'I'
+    elif total > 640 and total <= 1280:
+        designation = 'J'
+    elif total > 1280 and total <= 2560:
+        designation = 'K'
+    elif total > 2560 and total <= 5120:
+        designation = 'L'
+    elif total > 5120 and total <= 10240:
+        designation = 'M'
+    else:
+        designation = 'ERRO'
+        return designation
+
+    return f"{designation}{medio:.1f} - {tempo:.1f}"
+
+
+def save(file):
+    plt.savefig(file+'.png')
+
+    class PDF(FPDF):
+        def header(self):
+            # Logos
+            self.image(
+                '/home/viniciusmonnerat/Documentos/Serra_Rocketry/Analise de dados estaticos/LOGO - ALTERNATIVA.png', 210-47, -5, 50)
+            self.image(
+                '/home/viniciusmonnerat/Documentos/Serra_Rocketry/Analise de dados estaticos/logomarca-uerj-300x300.png', 2, 2, 35)
+
+            self.set_font('Arial', 'B', 25)
+            # Move to the right
+            self.cell(80)
+            # Title
+            self.cell(30, 25, 'Relatório de Teste Estático', 0, 0, 'C')
+            # Line break
+            self.ln(20)
+            self.set_fill_color(r=43, g=18, b=76)
+            self.set_y(45)
+            self.cell(0, 1, ' ', 0, 1, 'C', 1)
+
+        # Page footer
+        def footer(self):
+            # Position at 1.5 cm from bottom
+            self.set_y(-15)
+            # Arial italic 8
+            self.set_font('Arial', 'I', 8)
+            # Page number
+            self.cell(0, 10, 'Página ' + str(self.page_no()) +
+                      '/{nb}' + ' - Equipe Serra Rocketry', 0, 0, 'C')
+
+    # Instantiation of inherited class
+    pdf = PDF()
+    pdf.alias_nb_pages()
+    pdf.add_page()
+    pdf.set_xy(15, 50)
+    pdf.set_font('Times', 'B', 20)
+    pdf.cell(0, 10, list_result['nome'], 0, 0, 'C', 0)
+    pdf.set_xy(15, 60)
+    pdf.set_font('Times', '', 18)
+    pdf.multi_cell(0, 10, 'Data do teste: ' +
+                   str(list_result['data'])+'\nHora do teste: '+str(list_result['inicio']))
+
+    pdf.set_xy(15, 75)
+    pdf.set_font('Times', '', 14)
+    pdf.multi_cell(0, 8, '\nImpulso Total (N*s) = {:.3f}'.format(list_result['integral']) +
+                   '\nEmpuxo Médio (N) = {:.3f}'.format(list_result['medioE']) +
+                   '\nEmpuxo Máximo (N) = {:.3f}'.format(list_result['maxyE']) +
+                   '\nTempo aproximado de queima (s)= {:.1f}'.format(list_result['duracao']), 0, 1)
+
+    pdf.set_xy(105, 83)
+    pdf.set_font('Times', '', 50)
+    pdf.cell(0, 10, '{}'.format(list_result['classificacao']))
+
+    pdf.set_fill_color(r=43, g=18, b=76)
+    pdf.set_y(120)
+    pdf.cell(0, 1, ' ', 0, 1, 'C', 1)
+    pdf.image(file+'.png', (210/2)-90, 130, 180, 140)
+    pdf.output(file+'.pdf', 'F')
+
+    return None
+
+
+def select():
+
+    return easygui.fileopenbox()
+
+
+def save_path():
+
+    return easygui.filesavebox()
+
+
+def QU(y2, y1, x2, x1):
+    m = (y2 - y1) / (x2 - x1)
+    if m > 10 or m < -10:
+        if m < -10:
+            return None, None, 0
+        else:
+            return None, None, 1
+    else:
+        return y1, x1, 1
 
 
 def graph():
 
-    def save(file):
-        plt.savefig(file+'.png')
-
-        class PDF(FPDF):
-            def header(self):
-                # Logos
-                self.image(
-                    '/home/vinicius/Área de Trabalho/Serra_Rocketry/Analise de dados estaticos/LOGO - ALTERNATIVA.png', 210-47, -5, 50)
-                self.image(
-                    '/home/vinicius/Área de Trabalho/Serra_Rocketry/Analise de dados estaticos/logomarca-uerj-300x300.png', 2, 2, 35)
-
-                self.set_font('Arial', 'B', 25)
-                # Move to the right
-                self.cell(80)
-                # Title
-                self.cell(30, 25, 'Relatório de Teste Estático', 0, 0, 'C')
-                # Line break
-                self.ln(20)
-                self.set_fill_color(r=43, g=18, b=76)
-                self.set_y(45)
-                self.cell(0, 1, ' ', 0, 1, 'C', 1)
-
-            # Page footer
-            def footer(self):
-                # Position at 1.5 cm from bottom
-                self.set_y(-15)
-                # Arial italic 8
-                self.set_font('Arial', 'I', 8)
-                # Page number
-                self.cell(0, 10, 'Página ' + str(self.page_no()) +
-                          '/{nb}' + ' - Equipe Serra Rocketry', 0, 0, 'C')
-
-        # Instantiation of inherited class
-        pdf = PDF()
-        pdf.alias_nb_pages()
-        pdf.add_page()
-        pdf.set_xy(15, 50)
-        pdf.set_font('Times', '', 18)
-        pdf.multi_cell(0, 10, 'Data do teste: ' +
-                       str(list_result['data'])+'\nHora do teste: '+str(list_result['inicio']))
-
-        pdf.set_xy(15, 65)
-        pdf.set_font('Times', '', 14)
-        pdf.multi_cell(0, 8, '\nImpulso Total (N*s) = {:.3f}'.format(list_result['integral']) +
-                       '\nEmpuxo Médio (N) = {:.3f}'.format(list_result['medioE']) +
-                       '\nEmpuxo Máximo (N) = {:.3f}'.format(list_result['maxyE']) +
-                       '\nTempo aproximado de queima (s)= {:.1f}'.format(list_result['duracao']), 0, 1)
-
-        pdf.set_fill_color(r=43, g=18, b=76)
-        pdf.set_y(110)
-        pdf.cell(0, 1, ' ', 0, 1, 'C', 1)
-        pdf.image(file+'.png', (210/2)-90, 120, 180, 140)
-        pdf.output(file+'.pdf', 'F')
-
-        return None
-
-    def open_matplot():
-
-        return plt.show()
-
-    def save_path():
-
-        return easygui.filesavebox()
+    ctk.set_appearance_mode("dark")
+    root = ctk.CTk()
+    root.title("Painel de Análise")
+    root.maxsize(1400,  900)
+    dialog = ctk.CTkInputDialog(text="Qual o nome do motor?", title="Nome")
+    list_result['nome'] = dialog.get_input()
 
     sn = spline(xi, yi)
 
     plt.rcParams["figure.figsize"] = (8, 6)
     fig, ax1 = plt.subplots()
 
-    ax1.set_title('Gráfico Empuxo vs Tempo -' +
-                  list_result['data'] + '-' + list_result['inicio'], fontsize=15)
+    ax1.set_title(list_result['nome']+'\n', fontsize=15)
     ax1.grid()
     max_x = 0.0
     max_y = 0.0
@@ -166,11 +250,27 @@ def graph():
 
     linha.set_label('Interpolação')
     ax1.scatter(max_x, max_y, c='purple', label='Máximo')
-    ym = [(list_result['medioE'])]*(len(xi))
-    ax1.plot(xi, ym, c='green', label='Empuxo médio')
+    x2 = []
+    y2 = []
+    for i in range(len(yi)-1):
+        ytemp, xtemp, cond = QU(yi[i+1], yi[i], xi[i+1], xi[i])
+
+        if ytemp == None and cond == 0:
+            x2.append(xi[i])
+            y2.append(yi[i])
+            break
+        elif ytemp == None:
+            continue
+        else:
+            x2.append(xtemp)
+            y2.append(ytemp)
+
+    c0, c1 = mmq(x2, y2)
+    mmqy = [calc_mmq(ti, c1, c0) for ti in x2]
+    ax1.plot(x2, mmqy, c='blue', label=f'MMQ (A={c1:.4f}, B={c0:.4f})')
+
     ax1.scatter(xi, yi, c='black', label='Pontos')
     ax1.set_ylabel('Empuxo (N)', fontsize=12)
-    # ax1.set_xticks(xi, h, rotation=45)
     ax1.set_xlabel('Tempo (s)', fontsize=12)
     ax1.yaxis.set_minor_formatter('{x:.2f}')
     ax1.yaxis.set_major_formatter('{x:.2f}')
@@ -179,11 +279,6 @@ def graph():
     ax1.tick_params(which='major', length=7)
     ax1.tick_params(which='minor', length=4, color='gray')
     ax1.legend()
-
-    ctk.set_appearance_mode("dark")
-    root = ctk.CTk()
-    root.title("Painel de Análise")
-    root.maxsize(1400,  900)
 
     Frame = ctk.CTkFrame(master=root,  width=1200,  height=900,
                          fg_color='#473E66', corner_radius=0)
@@ -195,15 +290,14 @@ def graph():
     canvas = FigureCanvasTkAgg(fig, master=plotframe)
     canvas.draw()
     canvas.get_tk_widget().place(relx=0, rely=0)
-
     infos = ctk.CTkLabel(master=Frame, text='Data do teste: '+list_result['data'] +
                          '\nHorário do teste: '+list_result['inicio'] +
                          '\nImpulso Total (N*s) ≈ {:.3f}'.format(list_result['integral']) +
                          '\nEmpuxo médio (N) ≈ {:.3f}'.format(list_result['medioE']) +
                          '\nEmpuxo máximo (N) ≈ {:.3f}'.format(list_result['maxyE']) +
                          '\nQuantidade de pontos: '+list_result['pontos'] +
-                         '\nTempo de queima (s) ≈ {:.1f}'.format(
-                             list_result['duracao']),
+                         '\nTempo de queima (s) ≈ {:.1f}'.format(list_result['duracao']) +
+                         '\nClasse: {}'.format(list_result['classificacao']),
                          fg_color='#522B5B',
                          text_color='white',
                          corner_radius=5,
@@ -216,23 +310,14 @@ def graph():
                              hover_color='#2B124C', border_color='black', border_width=2, text='Gerar Relatório', command=lambda: [save(save_path())])
     save_btn.pack(padx=5, pady=10, side='top')
 
-    matplot_btn = ctk.CTkButton(master=Frame, corner_radius=10, height=50, width=150, font=('Roboto', 15), fg_color='#522B5B',
-                                hover_color='#2B124C', border_color='black', border_width=2, text='Abrir o Gráfico', command=lambda: [open_matplot()])
-    matplot_btn.pack(padx=5, pady=10, side='top')
-
     your_image = ctk.CTkImage(light_image=Image.open(
-        '/home/vinicius/Área de Trabalho/Serra_Rocketry/Analise de dados estaticos/LOGO - ALTERNATIVA.png'), size=(250, 250))
+        '/home/viniciusmonnerat/Documentos/Serra_Rocketry/Analise de dados estaticos/LOGO - ALTERNATIVA.png'), size=(250, 250))
     label = ctk.CTkLabel(master=Frame, image=your_image, text='')
     label.pack(padx=5, pady=5, side='bottom')
 
     root.update()
 
     return root.mainloop()
-
-
-def select():
-
-    return easygui.fileopenbox()
 
 
 yi = []
@@ -243,16 +328,16 @@ arq = open(select(), 'r').read()
 lines = arq.split('\n')
 for line in lines:
     if len(line) > 1:
-        d, z, x, y = line.split(';')
+        d, z, y, x = line.split(';')
         data.append(d)
         h.append(z)
-        yi.append(float(x)*9.81)
-        xii.append(float(y)/1000)
+        yi.append((float(y)+0.013) * 9.81)
+        xii.append(float(x) / 1000)
 
 xi = [xii[i]-xii[0] for i in range(len(xii))]
 
 list_result = {'integral': 0.0, 'maxxE': 0.0, 'maxyE': 0.0, 'medioE': 0.0,
-               'pontos': str(len(yi)), 'duracao': xi[len(xi)-1]-xi[0], 'inicio': h[0], 'data': data[0]}
+               'pontos': str(len(yi)), 'duracao': xi[len(xi)-1]-xi[0], 'inicio': h[0], 'data': data[0], 'classificacao': '', 'nome': ''}
 
 calc_integral()
 graph()
